ccd0ace38f784bb3ad6084f3481ab492de24e1b2
ApplePine <djl@applepine.cn>
2026-02-24 15:29:53 +0800

    fix: 修复路由未注册与elementplus样式未引入问题

--- template/src/index.ts
+++ template/src/index.ts
@@ -3,8 +3,11 @@ import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
+import router from './router'
+import 'element-plus/dist/index.css';
 
 const app = createApp(App);
 app.use(createPinia());
+app.use(router);
 app.use(ElementPlus);
 app.mount('#root');
