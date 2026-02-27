29ff93e1785cd7054a299c5f9e26065aa63000ee
ApplePine <djl@applepine.cn>
2026-02-27 09:34:01 +0800

    feat: 为模板接入 Element Plus 自动导入能力

--- template/rsbuild.config.ts
+++ template/rsbuild.config.ts
@@ -1,9 +1,27 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginVue } from '@rsbuild/plugin-vue';
+import AutoImport from 'unplugin-auto-import/rspack';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/rspack';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginVue()],
+  tools: {
+    rspack: {
+      plugins: [
+        AutoImport({
+          imports: ['vue'],
+          resolvers: [ElementPlusResolver()],
+          dts: './src/auto-imports.d.ts',
+        }),
+        Components({
+          resolvers: [ElementPlusResolver()],
+          dts: './src/components.d.ts',
+        }),
+      ],
+    },
+  },
   resolve: {
     alias: {
       '@': '/src',
